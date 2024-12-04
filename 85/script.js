e2fc108b6e1f512b535db1dcf708148d050832cd
Your Name <your_email@whatever.com>
2024-12-04 16:11:36 +0100

    85 promise

---
+++ 85/script.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// // console.log('Запрос данных...');
+
+// // setTimeout( () => {
+// //     console.log('Подготовка данных...');
+
+// //     const product = {
+// //         name: 'TV',
+// //         price: 200
+// //     };
+
+// //     setTimeout(() => {
+// //         product.status = 'order'; 
+// //         console.log(product);
+// //     }, 2000);
+// // }, 2000);
+
+
+// console.log('Запрос данных...');
+
+// const req = new Promise(function(resolve, reject) {
+
+//     setTimeout( () => {
+//         console.log('Подготовка данных...');
+    
+//         const product = {
+//             name: 'TV',
+//             price: 200
+//         };
+    
+//         resolve(product);
+//     }, 2000);
+
+// });
+
+
+// // req.then((product) => {
+// //     // console.log('Данные получены');
+// //     setTimeout(() => {
+// //         product.status = 'order'; 
+// //         console.log(product);
+// //     }, 2000);
+// // });
+
+
+// // req.then((product) => {
+// //     const req2 = new Promise((resolve, reject) => {
+// //         setTimeout(() => {
+// //                     product.status = 'order'; 
+// //                     resolve(product);
+// //                 }, 2000);
+// //     });
+
+// //     req2.then(data => {
+// //         console.log(data);
+// //     })
+// // });
+
+
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//                     product.status = 'order'; 
+//                     resolve(product);
+//                 }, 2000);
+//     });
+// }).then(data => {
+//     data.modify = true;
+//     return data;
+// }).then(data => {
+//     console.log(data);
+// }).catch(() => {
+//     console.error('Произошла ошибка');
+// }).finally(() => {
+//     console.log('finally');
+// });
+
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    });
+};
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('All');
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
+
